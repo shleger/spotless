842737e6ddb82cbe4da623893a646da7cb815a7b
shleger <sh2a@yandex.ru>
2013-04-10 00:12:39 +0400

    read file

--- src/main/java/SpotlessCarpet.java
+++ src/main/java/SpotlessCarpet.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: saa
@@ -7,26 +15,43 @@
  */
 public class SpotlessCarpet {
 
+    private BufferedReader bufferedReader;
+    private List<Integer[]> matrix;
 
-    public SpotlessCarpet(String dataFile){
 
+    public SpotlessCarpet(String dataFile) throws IOException {
+
+        bufferedReader = new BufferedReader(new FileReader(dataFile));
+        matrix = new ArrayList<Integer[]>();
+
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            Integer[] row = new Integer[line.length()];
+            for (int i = 0; i < line.length(); i++) {
+                row[i] = Integer.valueOf(line.charAt(i));
+            }
+            matrix.add(row);
+
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
-        if (args.length == 0){
+        if (args.length == 0) {
             System.out.println("usage: " + SpotlessCarpet.class.getSimpleName() + " dataFile");
             return;
         }
-
 
         SpotlessCarpet carpet = new SpotlessCarpet(args[0]);
         carpet.calcDrops();
 
     }
 
-    public void calcDrops() {
+    public void calcDrops()  {
+
+        System.out.println(matrix);
+
     }
 
 
