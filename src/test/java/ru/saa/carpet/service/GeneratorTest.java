c4885ff2130fd0e81537094400b38224eb20620d
shleger <sh2a@yandex.ru>
2013-04-12 04:03:47 +0400

    recurs multiplier

--- src/test/java/ru/saa/carpet/service/GeneratorTest.java
+++ src/test/java/ru/saa/carpet/service/GeneratorTest.java
@@ -4,7 +4,6 @@ import Jama.Matrix;
 import org.junit.Test;
 
 import java.text.NumberFormat;
-import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -37,9 +36,24 @@ public class GeneratorTest {
     @Test
     public void multipleMatrix1() {
 
-        double[][] mat = {{0, 1, 0, 0, 0}, {1, 0, 1, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 0}, {0, 1, 0, 0, 0}};
+//        double[][] mat = {{0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 1, 0, 0}, {0, 1, 0, 0, 0}};
+//        double[][] mat = {{0, 1, 0, 0, 0}, {1, 0, 1, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 0}, {0, 1, 0, 0, 0}}; //OR
+//        double[][] mat = {
+//                {0, 1, 0, 0, 0},
+//                {1, 0, 1, 0, 0},
+//                {0, 1, 0, 1, 0},
+//                {0, 0, 1, 0, 1},
+//                {0, 0, 0, 1, 0}};  //LINE
+        double[][] mat = {
+                {0, 1, 1, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0},
+                {1, 0, 0, 1, 1, 1},
+                {0, 0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0},
+        };  //LINE2
 
-        Matrix a = new Matrix(mat, 5, 5);
+        Matrix a = new Matrix(mat, mat.length, mat.length);
         Matrix fin = null;
         printMatrix(a);
 
@@ -55,11 +69,55 @@ public class GeneratorTest {
         Matrix a5 = a4.times(a);
         printMatrix(a5);
 
+        Matrix a6 = a5.times(a);
+        printMatrix(a6);
 
-        fin = a.plus(a2).plus(a3).plus(a4).plus(a5);
+
+        System.out.println("fin:");
+        fin = a.plus(a2).plus(a3).plus(a4).plus(a5).plus(a6);
+        printMatrix(fin);
+
+    }
+
+    @Test
+    public void multipleMatrixSpoon() {
 
 
+        double[][] mat = {
+                {0, 1, 1, 1, 0, 0, 0},
+                {1, 0, 0, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0, 1, 0},
+                {1, 0, 0, 0, 0, 0, 1},
+                {0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0},
+        };  //SPOON
 
+        Matrix a = new Matrix(mat, mat.length, mat.length);
+        Matrix fin = null;
+        printMatrix(a);
+
+        Matrix a2 = a.times(a);
+        printMatrix(a2);
+
+        Matrix a3 = a2.times(a);
+        printMatrix(a3);
+
+        Matrix a4 = a3.times(a);
+        printMatrix(a4);
+
+        Matrix a5 = a4.times(a);
+        printMatrix(a5);
+
+        Matrix a6 = a5.times(a);
+        printMatrix(a6);
+
+        Matrix a7 = a6.times(a);
+        printMatrix(a7);
+
+
+        System.out.println("fin:");
+        fin = a.plus(a2).plus(a3).plus(a4).plus(a5).plus(a6).plus(a7);
         printMatrix(fin);
 
     }
@@ -122,7 +180,7 @@ public class GeneratorTest {
 
         Matrix a = new Matrix(mat, 4, 4);
         Matrix b = new Matrix(mat, 4, 4);
-        Matrix fin = new Matrix(4, 4);
+        Matrix fin = null;
         printMatrix(a);
 
         Matrix a2 = a.times(b);
@@ -139,12 +197,46 @@ public class GeneratorTest {
 
         printMatrix(fin);
 
+        System.out.println("create:-------");
+        fin = buildReachabilityMatrix(a);
+
+        printMatrix(fin);
+
     }
 
-   private static NumberFormat nf = NumberFormat.getIntegerInstance();
+    private static NumberFormat nf = NumberFormat.getIntegerInstance();
+
     private void printMatrix(Matrix m) {
         System.out.println("------------------");
-        m.print(nf,m.getRowDimension());
+        m.print(nf, m.getRowDimension());
         System.out.println("------------------");
     }
+
+    private int exp = 0;
+    Matrix mul = null;
+    Matrix sum = null;
+    Matrix multiplier = null;
+
+    public Matrix buildReachabilityMatrix(Matrix m) {
+
+        if(exp==0) {
+            multiplier = m.copy();
+            sum = multiplier.copy();
+        }
+
+        printMatrix(m);
+        if (exp != m.getRowDimension() - 1 ) {
+            exp++;
+            mul = m.times(multiplier);
+            sum = sum.plus(mul);
+            buildReachabilityMatrix(mul);
+        }
+        return sum;
+
+    }
+
+//    private Matrix involution(Matrix m, int exponent){
+//        return m.times(m);
+//
+//    }
 }
